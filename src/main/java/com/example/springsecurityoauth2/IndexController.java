e7f7a61de2140d458ad75343a136ea38087eb2aa
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2024-06-28 21:22:10 +0900

    feat: keycloakClientRegistrationRepository

--- src/main/java/com/example/springsecurityoauth2/IndexController.java
+++ src/main/java/com/example/springsecurityoauth2/IndexController.java
@@ -1,13 +1,30 @@
 package com.example.springsecurityoauth2;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    @Autowired
+    private ClientRegistrationRepository clientRegistrationRepository;
+
     @GetMapping("/")
     public String index() {
+
+        ClientRegistration keycloakClientRegistration = clientRegistrationRepository.findByRegistrationId("keycloak");
+
+        String clientId = keycloakClientRegistration.getClientId();
+        System.out.println("clientId = " + clientId);
+
+        String redirectUri = keycloakClientRegistration.getRedirectUri();
+        System.out.println("redirectUri = " + redirectUri);
+
+
         return "index";
     }
 }
---
+++ src/main/java/com/example/springsecurityoauth2/OAuth2ClientConfig.java
@@ -0,0 +1,28 @@
+package com.example.springsecurityoauth2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.ClientRegistrations;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+
+@Configuration
+public class OAuth2ClientConfig {
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(this.keycloakClientRegistration());
+
+    }
+
+    private ClientRegistration keycloakClientRegistration() {
+        return ClientRegistrations.fromIssuerLocation("http://localhost:8080/realms/oauth2")
+                .registrationId("keycloak")
+                .clientId("oauth2-client-app")
+                .clientSecret("0BVSwKvJ70crOjNs8hSDfXbC7Vwi8KLL")
+                .scope("openid", "profile", "email")
+                .redirectUri("http://localhost:8081/login/oauth2/code/keycloak")
+                .build();
+    }
+}
